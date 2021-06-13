3d9d865ec5ffc955b84f7cd86148059b0b66f609
MANISH SINGH <manishsingh332000@gmail.com>
2021-06-13 07:09:35 +0530

    backend TA

---
+++ src/pages/SolveDoubt.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Topbar from '../components/Topbar/Topbar';
+import TeacherAssistance from '../components/User/TeacherAssistance';
+
+function SolveDoubt(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default SolveDoubt;
