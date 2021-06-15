58532d73fefaa1d023c63bbfda6dab1720caad63
MANISH SINGH <manishsingh332000@gmail.com>
2021-06-15 15:02:39 +0530

    teacher section styling

--- src/components/User/TeacherDashboard.jsx
+++ src/components/User/TeacherDashboard.jsx
@@ -3,14 +3,26 @@ import axios from "axios";
 
 function TeacherDashboard(props) {
     const [teachers, setTeachers]  = useState([]);
+   
     useEffect(() => {
         const fetchteachers = async() => {
             const res = await axios.get('/auth/only/TA');
             setTeachers(res.data);
             console.log(res.data);
+        
         }
         fetchteachers();
     },[])
+
+    const TimeCalculate = ({time}) => {
+            let total =0;
+            time.map((t)=> (
+                total+=parseFloat(t)
+            ))
+            return (
+                <span className="listItem"> {total} &nbsp; Average Doubt Activity Time</span>
+            )
+    }
     return (
         <div className = "solveDoubtContainer">
             <div className="teacherContainer">
@@ -31,18 +43,27 @@ function TeacherDashboard(props) {
                 <h2>Report of TA</h2>
                 <div className="reportContainer">
 
-                    <div className = "reportList">
+                   <ul className = "unorderList">
                         {
                             teachers.map((t)=> (
-                                <>
-                                <span className="listItem">{t.username}</span>
-                                <span className="listItem">{t.accepted?.length} Accepted</span>
-                                <span className="listItem">{t.escalated?.length} Escalted</span>
-                                <span>{t.accepted?.length- t.escalated?.length} total ANswer</span>
-                                </>
+                                <li className = "reportList">
+                                    <div className="name">
+                                        <h4>{t.username}</h4>
+                                    </div>
+                                        
+                                    <div className="reportName"> 
+                                        <span className="listItem">{t.accepted?.length} &nbsp; Doubts Accepted</span>
+                                        <span className="listItem">{t.escalated?.length} &nbsp; Doubts Escalted</span>
+                             
+                                        <span className="listItem">{t.accepted?.length- t.escalated?.length} &nbsp; Doubts Answered</span>
+                                        <TimeCalculate time = {t.answerTime}/>
+                                        
+                                    </div>
+                               
+                                </li>
                             ))
                         }
-                    </div>
+                    </ul>
                 </div>
             
             </div>
