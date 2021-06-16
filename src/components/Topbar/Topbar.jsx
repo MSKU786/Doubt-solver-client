f88a2db6f017bb1053aa0ee2719a6b33e67678ba
MANISH SINGH <manishsingh332000@gmail.com>
2021-06-16 18:21:35 +0530

    Complete I think

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/auth';
-import { Link, useHistory } from "react-router-dom";
+import { Link} from "react-router-dom";
 import BugReportIcon from '@material-ui/icons/BugReport';
 import "./topbar.css"
 function Topbar(props) {
-    const history = useHistory();
+
     const {user} = useContext(AuthContext);
     const logoutHandler = () => {
-        localStorage.clear();
-        history.push('/');
+        const deleteToken = async() => {
+            await localStorage.clear();
+        }
+        deleteToken();
+        window.location.reload();
     }
     const StudentBar = () => {
         return (
