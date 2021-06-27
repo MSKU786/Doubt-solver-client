cc07ee4f179dee5771a5d0f04b1b988bc577b8d4
MANISH SINGH <manishsingh332000@gmail.com>
2021-06-27 16:13:02 +0530

    Google auth

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("https://doubtsolverbackend.herokuapp.com/api/auth/login", userCredential);
+    const res = await axios.post("/auth/login", userCredential);
     localStorage.setItem('token', res.data.token);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data.user});
   } catch (err) {
@@ -14,5 +14,5 @@ export const loginCall = async (userCredential, dispatch) => {
 };
 
 
-const serverId = "https://doubtsolverbackend.herokuapp.com/api"
+const serverId = "http://localhost:8000/api"
 export default serverId
